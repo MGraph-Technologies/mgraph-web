ef8198d4981e3cded71f6210ed273cb06a8c3664
Rob Dearborn <rfdearborn@users.noreply.github.com>
2022-07-08 09:49:12 -0400

    whoops: local baseUrl actually is needed, but should be using with in workflow

--- cypress.config.ts
+++ cypress.config.ts
@@ -7,6 +7,7 @@ require('dotenv').config()
 
 export default defineConfig({
   e2e: {
+    baseUrl: 'http://localhost:3000',
     chromeWebSecurity: false,
     screenshotOnRunFailure: false,
     video: false,
