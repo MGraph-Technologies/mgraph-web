141405e8d88f64764e2e50648a96afe7c19e49e2
Rob Dearborn <rfdearborn@users.noreply.github.com>
2022-08-09 07:23:10 -0400

    attempting to view non-own-org page returns null

--- pages/[organizationName]/metrics/[metricId].tsx
+++ pages/[organizationName]/metrics/[metricId].tsx
@@ -4,6 +4,7 @@ import { FunctionComponent, useEffect, useState } from "react"
 import { EditText, EditTextarea } from 'react-edit-text'
 import 'react-edit-text/dist/index.css'
 
+import { useAuth } from '../../../contexts/auth'
 import { useEditability } from '../../../contexts/editability'
 import styles from '../../../styles/MetricDetail.module.css'
 import { supabase } from '../../../utils/supabaseClient'
@@ -12,6 +13,7 @@ type MetricDetailProps = {}
 const MetricDetail: FunctionComponent<MetricDetailProps> = () => {
   const router = useRouter()
   const { organizationName, metricId } = router.query
+  const { organizationName: userOrganizationName } = useAuth()
 
   const [name, setName] = useState('')
   const [description, setDescription] = useState('')
@@ -50,69 +52,73 @@ const MetricDetail: FunctionComponent<MetricDetailProps> = () => {
   }, [metricId])
 
   return (
-    <div className={styles.container}>
-      <div className={styles.header}>
-        <Button
-          className="p-button-text"
-          icon="pi pi-angle-left"
-          onClick={() => {
-            router.push('/' + organizationName)
-          }}
-        />
-        <h1>
-          <EditText
-            value={name}
+    organizationName === userOrganizationName ? (
+      <div className={styles.container}>
+        <div className={styles.header}>
+          <Button
+            className="p-button-text"
+            icon="pi pi-angle-left"
+            onClick={() => {
+              router.push('/' + organizationName)
+            }}
+          />
+          <h1>
+            <EditText
+              value={name}
+              readonly={!editingEnabled}
+              onChange={(e) => setName(e.target.value)}
+              // onSave={saveProperties}
+            />
+          </h1>
+        </div>
+        <div className={styles.detail_field}>
+          Chart TBA
+        </div>
+        <h2>Description</h2>
+        <div className={styles.detail_field}>
+          <EditTextarea
+            value={description}
             readonly={!editingEnabled}
-            onChange={(e) => setName(e.target.value)}
+            placeholder={ editingEnabled ? 'Add...' : '-' }
+            style={{ backgroundColor: editingEnabled ? '#EEE': '#F8F8F8' }}
+            onChange={(e) => setDescription(e.target.value)}
             // onSave={saveProperties}
           />
-        </h1>
+        </div>
+        <h2>Inputs</h2>
+        <div className={styles.detail_field}>
+          Inputs TBA
+        </div>
+        <h2>Outputs</h2>
+        <div className={styles.detail_field}>
+          Outputs TBA
+        </div>
+        <h2>Owner</h2>
+        <div className={styles.detail_field}>
+          <EditText
+            value={owner}
+            readonly={!editingEnabled}
+            placeholder={ editingEnabled ? 'Add...' : '-' }
+            style={{ backgroundColor: editingEnabled ? '#EEE': '#F8F8F8' }}
+            onChange={(e) => setOwner(e.target.value)}
+            // onSave={saveProperties}
+          />
+        </div>
+        <h2>Source</h2>
+        <div className={styles.detail_field}>
+          <EditTextarea
+            value={source}
+            readonly={!editingEnabled}
+            placeholder={ editingEnabled ? 'Add...' : '-' }
+            style={{ backgroundColor: editingEnabled ? '#EEE': '#F8F8F8' }}
+            onChange={(e) => setSource(e.target.value)}
+            // onSave={saveProperties}
+          />
+        </div>
       </div>
-      <div className={styles.detail_field}>
-        Chart TBA
-      </div>
-      <h2>Description</h2>
-      <div className={styles.detail_field}>
-        <EditTextarea
-          value={description}
-          readonly={!editingEnabled}
-          placeholder={ editingEnabled ? 'Add...' : '-' }
-          style={{ backgroundColor: editingEnabled ? '#EEE': '#F8F8F8' }}
-          onChange={(e) => setDescription(e.target.value)}
-          // onSave={saveProperties}
-        />
-      </div>
-      <h2>Inputs</h2>
-      <div className={styles.detail_field}>
-        Inputs TBA
-      </div>
-      <h2>Outputs</h2>
-      <div className={styles.detail_field}>
-        Outputs TBA
-      </div>
-      <h2>Owner</h2>
-      <div className={styles.detail_field}>
-        <EditText
-          value={owner}
-          readonly={!editingEnabled}
-          placeholder={ editingEnabled ? 'Add...' : '-' }
-          style={{ backgroundColor: editingEnabled ? '#EEE': '#F8F8F8' }}
-          onChange={(e) => setOwner(e.target.value)}
-          // onSave={saveProperties}
-        />
-      </div>
-      <h2>Source</h2>
-      <div className={styles.detail_field}>
-        <EditTextarea
-          value={source}
-          readonly={!editingEnabled}
-          placeholder={ editingEnabled ? 'Add...' : '-' }
-          style={{ backgroundColor: editingEnabled ? '#EEE': '#F8F8F8' }}
-          onChange={(e) => setSource(e.target.value)}
-          // onSave={saveProperties}
-        />
-      </div>
-    </div>
+    ) : (
+      null
+    )
   )
 }
 
