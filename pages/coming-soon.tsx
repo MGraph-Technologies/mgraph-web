bdf94c3641fee5cfce0a945a333762a4e9cb0bbc
Rob Dearborn <rfdearborn@users.noreply.github.com>
2022-09-26 23:48:03 -0400

    lint

--- pages/coming-soon.tsx
+++ pages/coming-soon.tsx
@@ -13,8 +13,8 @@ const ComingSoon: FunctionComponent<Props> = () => {
       </Head>
       <Header />
       <p>
-        Thanks for your interest in MGraph — you&#39;ve been added to our waitlist and 
-        we&#39;ll be in touch soon!
+        Thanks for your interest in MGraph — you&#39;ve been added to our
+        waitlist and we&#39;ll be in touch soon!
       </p>
     </div>
   )
