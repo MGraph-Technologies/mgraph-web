42bb766eb82c6cc17b46e473013186fb8c683d95
Rob Dearborn <rfdearborn@users.noreply.github.com>
2022-10-02 23:05:09 -0400

    overlay last non-null number value

--- components/LineChart.tsx
+++ components/LineChart.tsx
@@ -144,7 +144,8 @@ const LineChart: FunctionComponent<LineChartProps> = ({ queryResult }) => {
         )
         const numberToOverlay =
           datasets.length === 1
-            ? Number(datasets[0].data[datasets[0].data.length - 1].y)
+            // last non-null value
+            ? Number(datasets[0].data.reverse().find((d) => d.y !== null)?.y)
             : null
         const numberToOverlayString =
           numberToOverlay !== null
