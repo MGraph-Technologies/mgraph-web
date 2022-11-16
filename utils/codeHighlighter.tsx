e2c9e6a3e877dd946cb61f21f6713e6508cc3bd9
Rob Dearborn <rfdearborn@users.noreply.github.com>
2022-11-15 20:00:05 -0500

    Allow admins to set generatedQueryTemplate

---
+++ utils/codeHighlighter.tsx
@@ -0,0 +1,22 @@
+import hljs from 'highlight.js/lib/core'
+import plaintext from 'highlight.js/lib/languages/plaintext'
+import sql from 'highlight.js/lib/languages/sql'
+import yaml from 'highlight.js/lib/languages/yaml'
+import 'highlight.js/styles/docco.css'
+
+hljs.registerLanguage('plaintext', plaintext)
+hljs.registerLanguage('sql', sql)
+hljs.registerLanguage('yaml', yaml)
+
+// https://github.com/highlightjs/highlight.js/issues/925
+export const highlight = (code: string, language: string) => {
+  return (
+    <code
+      className="hljs"
+      dangerouslySetInnerHTML={{
+        __html: hljs.highlight(code, { language }).value,
+      }}
+      style={{ borderRadius: '5px' }}
+    />
+  )
+}
