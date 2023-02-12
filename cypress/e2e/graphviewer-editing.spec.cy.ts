9024fb565c9c946bae598cb9916daa1b7ee958f8
Rob Dearborn <rfdearborn@users.noreply.github.com>
2023-02-12 07:25:05 -0500

    Whoops

--- cypress/e2e/graphviewer-editing.spec.cy.ts
+++ cypress/e2e/graphviewer-editing.spec.cy.ts
@@ -6,87 +6,87 @@ describe('Graphviewer editing', () => {
     )
   })
 
-  // it('Adds a custom node, then cancels addition', () => {
-  //   cy.visit('/mgraph')
-  //   /* wait for graph to load before editing
-  //   (otherwise, added nodes are overwritten by graph load) */
-  //   cy.wait(2000)
+  it('Adds a custom node, then cancels addition', () => {
+    cy.visit('/mgraph')
+    /* wait for graph to load before editing
+    (otherwise, added nodes are overwritten by graph load) */
+    cy.wait(2000)
 
-  //   // begin editing
-  //   cy.get('[id=edit-button]').click()
+    // begin editing
+    cy.get('[id=edit-button]').click()
 
-  //   // add and rename metric
-  //   const newNodeName = Math.random().toString(36)
-  //   cy.get('[id=add-node-button]').click()
-  //   cy.get('[class*=p-listbox-item]').contains('Custom').click()
-  //   cy.get('.react-flow__controls-fitview').click()
-  //   cy.get('.react-flow__node-custom').contains('New Custom').click()
-  //   cy.get('input').first().clear().type(newNodeName).type('{enter}')
-  //   cy.get('.react-flow__node-custom')
-  //     .contains(newNodeName)
-  //     .should('be.visible')
+    // add and rename metric
+    const newNodeName = Math.random().toString(36)
+    cy.get('[id=add-node-button]').click()
+    cy.get('[class*=p-listbox-item]').contains('Custom').click()
+    cy.get('.react-flow__controls-fitview').click()
+    cy.get('.react-flow__node-custom').contains('New Custom').click()
+    cy.get('input').first().clear().type(newNodeName).type('{enter}')
+    cy.get('.react-flow__node-custom')
+      .contains(newNodeName)
+      .should('be.visible')
 
-  //   // cancel
-  //   cy.get('[id=cancel-button]').click()
-  //   cy.get('.react-flow__node-custom').contains(newNodeName).should('not.exist')
-  // })
+    // cancel
+    cy.get('[id=cancel-button]').click()
+    cy.get('.react-flow__node-custom').contains(newNodeName).should('not.exist')
+  })
 
-  // it('Adds a metric, tests undo and redo, adds a formula, then cancels additions', () => {
-  //   cy.visit('/mgraph')
-  //   /* wait for graph to load before editing
-  //   (otherwise, added nodes are overwritten by graph load) */
-  //   cy.wait(2000)
+  it('Adds a metric, tests undo and redo, adds a formula, then cancels additions', () => {
+    cy.visit('/mgraph')
+    /* wait for graph to load before editing
+    (otherwise, added nodes are overwritten by graph load) */
+    cy.wait(2000)
 
-  //   // begin editing
-  //   cy.get('[id=edit-button]').click()
+    // begin editing
+    cy.get('[id=edit-button]').click()
 
-  //   // add and rename metric
-  //   const newMetricName = Math.random().toString(36)
-  //   cy.get('[id=add-node-button]').click()
-  //   cy.get('[class*=p-listbox-item]').contains('Metric').click()
-  //   cy.get('.react-flow__controls-fitview').click()
-  //   cy.get('.react-flow__node-metric').contains('New Metric').click()
-  //   cy.get('input').first().clear().type(newMetricName).type('{enter}')
-  //   cy.get('.react-flow__node-metric')
-  //     .contains(newMetricName)
-  //     .should('be.visible')
+    // add and rename metric
+    const newMetricName = Math.random().toString(36)
+    cy.get('[id=add-node-button]').click()
+    cy.get('[class*=p-listbox-item]').contains('Metric').click()
+    cy.get('.react-flow__controls-fitview').click()
+    cy.get('.react-flow__node-metric').contains('New Metric').click()
+    cy.get('input').first().clear().type(newMetricName).type('{enter}')
+    cy.get('.react-flow__node-metric')
+      .contains(newMetricName)
+      .should('be.visible')
 
-  //   // undo
-  //   cy.get('[id=undo-button]').click()
-  //   cy.get('.react-flow__node-metric')
-  //     .contains(newMetricName)
-  //     .should('not.exist')
+    // undo
+    cy.get('[id=undo-button]').click()
+    cy.get('.react-flow__node-metric')
+      .contains(newMetricName)
+      .should('not.exist')
 
-  //   // redo
-  //   cy.get('[id=redo-button]').click()
-  //   cy.get('.react-flow__node-metric')
-  //     .contains(newMetricName)
-  //     .should('be.visible')
+    // redo
+    cy.get('[id=redo-button]').click()
+    cy.get('.react-flow__node-metric')
+      .contains(newMetricName)
+      .should('be.visible')
 
-  //   // add formula
-  //   cy.get('[id=add-relationship-button]').click()
-  //   cy.get('[class*=p-listbox-item]').contains('Formula').click()
-  //   cy.get('[id=formula-field]')
-  //     .click()
-  //     .type(newMetricName)
-  //     .wait(2000)
-  //     .type('{enter}') // wait for suggestion to load
-  //     .type('~')
-  //     .wait(2000)
-  //     .type('{enter}')
-  //     .type('Active Users')
-  //     .wait(2000)
-  //     .type('{enter}')
-  //   cy.get('[id=save-formula-button]').click()
-  //   // TODO: check newly-added function node is visible
-  //   // (requires distinguishing which function node is the new one)
+    // add formula
+    cy.get('[id=add-relationship-button]').click()
+    cy.get('[class*=p-listbox-item]').contains('Formula').click()
+    cy.get('[id=formula-field]')
+      .click()
+      .type(newMetricName)
+      .wait(2000)
+      .type('{enter}') // wait for suggestion to load
+      .type('~')
+      .wait(2000)
+      .type('{enter}')
+      .type('Active Users')
+      .wait(2000)
+      .type('{enter}')
+    cy.get('[id=save-formula-button]').click()
+    // TODO: check newly-added function node is visible
+    // (requires distinguishing which function node is the new one)
 
-  //   // cancel
-  //   cy.get('[id=cancel-button]').click()
-  //   cy.get('.react-flow__node-metric')
-  //     .contains(newMetricName)
-  //     .should('not.exist')
-  // })
+    // cancel
+    cy.get('[id=cancel-button]').click()
+    cy.get('.react-flow__node-metric')
+      .contains(newMetricName)
+      .should('not.exist')
+  })
 
   it('Edits table view order', () => {
     cy.visit('/mgraph')
