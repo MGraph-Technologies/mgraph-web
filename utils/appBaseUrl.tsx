1fd0dda7b5114f479b08dad7baa34a655e49dc01
Rob Dearborn <rfdearborn@users.noreply.github.com>
2023-04-27 12:36:53 -0400

    Establish NEXT_PUBLIC_PROD_BASE_URL

--- utils/appBaseUrl.tsx
+++ utils/appBaseUrl.tsx
@@ -1,6 +1,6 @@
 export const getBaseUrl: () => string = () => {
   return process.env.NEXT_PUBLIC_ENV === 'production'
-    ? 'https://app.mgraph.us'
+    ? process.env.NEXT_PUBLIC_PROD_BASE_URL || ''
     : process.env.NEXT_PUBLIC_ENV === 'staging'
     ? `https://${process.env.VERCEL_URL}`
     : 'http://localhost:3000'
